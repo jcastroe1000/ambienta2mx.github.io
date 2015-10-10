1985b3c119afda6cc39ff479d220a936802de4bd
Azucena López <blankazucenalg@gmail.com>
2015-10-10 01:46:48 -0500

    Github integration. Event types described for each posible case.

--- js/github-integration.js
+++ js/github-integration.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
      $.getJSON("https://api.github.com/orgs/Ambienta2MX/events", function(json) {
        if (json.message != "Not Found"){
          var output = "<table class='table table-hover table-striped'><tr><th>Evento</th><th>Realizado por</th><th>Módulo</th><th>Fecha</th></tr>";
-         for( i = 0; i < 7 ; i++){
+         for( i = 0; i < 10 ; i++){
            //if(json[i].actor.login == 'jresendiz27' || json[i].actor.login == 'egjimenezg'){
              switch (json[i].type) {
               case "CommitCommentEvent":
@@ -17,28 +17,31 @@ $(document).ready(function(){
                 output += "<tr><td>Deleted a "+json[i].payload.ref_type;
                 break;
               case "DeploymentEvent":
-                output += "<tr><td>Deployment";
+                output += "<tr><td>Deployment to the "+json[i].payload.environment+" environment";
                 break;
               case "DeploymentStatusEvent":
                 output += "<tr><td>Deployment status: "+json[i].payload.deplyment_status.state;
                 break;
               case "DownloadEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>A new download was created";
                 break;
               case "FollowEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>"+json[i].payload.target.login+" was followed";
                 break;
               case "ForkEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>The repository <b>"+json[i].payload.forkee.name+"</b> was forked.";
                 break;
               case "ForkApplyEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>A patch was applied in the Fork Queue to the branch <b>"+json[i].payload.head+"</b>";
                 break;
               case "GistEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>A Gist was "+json[i].payload.action;
                 break;
               case "GollumEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>Wiki pages changed. ";
+                for(j=0; j < json[i].payload.pages.length; j++){
+                  output += "<b>"+json[i].payload.pages[j].page_name + "</b> was "+json[i].payload.pages[j].action;
+                }
                 break;
               case "IssueCommentEvent":
                 output += "<tr><td>Commented the issue <b>"+json[i].payload.issue.title+"</b> <i>\""+json[i].payload.comment.body+"\"</i>";
@@ -47,40 +50,40 @@ $(document).ready(function(){
                 output += "<tr><td>The issue <b>"+json[i].payload.issue.title+"</b> was "+json[i].payload.action;
                 break;
               case "MemberEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>The user <b>"+json[i].payload.member.login+"</b> was added as a collaborator to the repository.";
                 break;
               case "MembershipEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>The user <b>"+json[i].payload.member.login+"</b> was "+json[i].payload.action+" from the team "+ json[i].payload.team.name;
                 break;
               case "PageBuildEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>There was an attempted build of a GitHub Pages site";
                 break;
               case "PublicEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>The private repository is now open sourced";
                 break;
               case "PullRequestEvent":
                 output += "<tr><td>A pull request was "+json[i].payload.action+": <i>\""+json[i].payload.pull_request.title+"\"</i>";
                 break;
               case "PullRequestReviewCommentEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>A comment was created on a portion of the unified diff of a pull request. <i>\""+json[i].payload.comment.body+"\"</i>";
                 break;
               case "PushEvent":
                 output += "<tr><td>Pushed on <b>"+json[i].payload.ref.replace('refs/heads/','')+"</b> Last commit: <i>\""+json[i].payload.commits[0].message+"\"</i>";
                 break;
               case "ReleaseEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>A <b>release</b> was published.";
                 break;
               case "RepositoryEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>The repository <b>"+json[i].payload.repository.name+"</b> was created.";
                 break;
               case "StatusEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>The status of the commit <i>\""+json[i].payload.commit.message+"\"</i> changed to <b>"+json[i].payload.state+"</b>";
                 break;
               case "TeamAddEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>The repository was added to the team <b>"+json[i].payload.team.name+"</b>.";
                 break;
               case "WatchEvent":
-                output += "<tr><td>"+json[i].type;
+                output += "<tr><td>"+json[i].payload.sender.login+" had started watching (starring) the repository.";
                 break;
               default:
                 output += "<tr><td>"+json[i].type;
